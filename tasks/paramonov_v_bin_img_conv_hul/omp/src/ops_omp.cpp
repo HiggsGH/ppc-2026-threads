1b10c71d4c861c2efea31b073b9911d9a4fae1e2
Tripxp1 <144802453+Tripxp1@users.noreply.github.com>
2026-03-20 17:26:51 +0300

    Add files via upload

--- tasks/paramonov_v_bin_img_conv_hul/omp/src/ops_omp.cpp
+++ tasks/paramonov_v_bin_img_conv_hul/omp/src/ops_omp.cpp
@@ -173,10 +173,12 @@ void ConvexHullOMP::ExtractConnectedComponents() {
 
   FindStartPoints(pixels, rows, cols, visited, start_points);
 
-#pragma omp parallel for default(none) shared(start_points, total_pixels, rows, cols, pixels, components)
-  for (size_t i = 0; i < start_points.size(); ++i) {
-    int start_row = start_points[i].first;
-    int start_col = start_points[i].second;
+  const size_t num_points = start_points.size();
+#pragma omp parallel for default(none) shared(num_points, start_points, total_pixels, rows, cols, pixels, components)
+  for (size_t i = 0; i < num_points; ++i) {
+    const auto &start_point = start_points[i];
+    int start_row = start_point.first;
+    int start_col = start_point.second;
     ProcessComponent(start_row, start_col, rows, cols, total_pixels, pixels, components);
   }
 
