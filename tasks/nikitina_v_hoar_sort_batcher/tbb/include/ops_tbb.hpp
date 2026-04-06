b6f31043af008bf445e262a1215703a6a8ab8d91
Лера Никитина <valerianikitina56436@gmail.com>
2026-04-06 19:47:35 +0000

    add tbb

---
+++ tasks/nikitina_v_hoar_sort_batcher/tbb/include/ops_tbb.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <vector>
+
+#include "nikitina_v_hoar_sort_batcher/common/include/common.hpp"
+
+namespace nikitina_v_hoar_sort_batcher {
+
+class HoareSortBatcherTBB : public BaseTask {
+ public:
+  static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() {
+    return ppc::task::TypeOfTask::kTBB;
+  }
+
+  explicit HoareSortBatcherTBB(const InType &in);
+
+ private:
+  bool ValidationImpl() override;
+  bool PreProcessingImpl() override;
+  bool RunImpl() override;
+  bool PostProcessingImpl() override;
+};
+
+}  // namespace nikitina_v_hoar_sort_batcher
